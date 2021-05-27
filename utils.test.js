0163022fad9cb454f7461245f7a80bd56134c2ce
Matt Stow <matt@mattstow.com>
2021-05-27 22:13:24 +1000

    Simplify to improve performance

--- utils.test.js
+++ utils.test.js
@@ -1,5 +1,5 @@
 const utils = require("./utils");
 
 test("#ffffff converts to rgb object", () => {
-  expect(utils.hexToRGB("#ffffff")).toStrictEqual({ R: 255, G: 255, B: 255 });
+  expect(utils.hexToRGB("#ffffff")).toStrictEqual({ r: 255, g: 255, b: 255 });
 });
