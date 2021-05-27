00bb54bf21faab3accf027d74faa374bcf92667f
Nathan Simpson <hello@nathansimpson.design>
2021-05-27 16:35:23 +1000

    Add support for shorthand hex notation

--- index.test.js
+++ index.test.js
@@ -4,7 +4,7 @@ test("#FFFFFF converts to rgb(255,255,255)", () => {
   expect(hexAlpha("#ffffff")).toBe("rgb(255,255,255)");
 });
 
-test("#000000 converts to rgb(0,0,0,1)", () => {
+test("#000000 converts to rgba(0,0,0,1)", () => {
   expect(hexAlpha("#000000", 1)).toBe("rgba(0,0,0,1)");
 });
 
@@ -18,4 +18,8 @@ test("HEX color and alpha converts to RGBA", () => {
 
 test("HEX color with zero alpha converts to RGBA", () => {
   expect(hexAlpha("#fa6d01", 0)).toBe("rgba(250,109,1,0)");
+});
+
+test("#f00 converts to rgba(0,0,0,1)", () => {
+  expect(hexAlpha("#f00", 0.1)).toBe("rgba(255,0,0,0.1)");
 });
