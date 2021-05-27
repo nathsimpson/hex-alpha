6836e5c75752fabfe33c3aa3ade143223767104f
Jeremy Grancher <jgrancher@gmail.com>
2021-05-27 16:58:48 +1000

    Enforcing alpha >= 0 & alpha <= 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { hexToRGB, formatRGB, formatRGBA } = require("./utils");
 const hexAlpha = (input, alpha) => {
   const newColor = hexToRGB(input);
 
-  return alpha && typeof alpha === "number"
+  return alpha && typeof alpha === "number" && alpha >= 0 && alpha <= 1
     ? formatRGBA(newColor, alpha)
     : formatRGB(newColor);
 };
